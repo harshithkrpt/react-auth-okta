55f86c5a811843b48778e1ce17ae5fbae6769e37
Harshith Kurapati <harshith.krpt@gmail.com>
2018-10-07 13:24:56 +0530

    have logout problems

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Layout/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
 import Home from "./components/Pages/Home";
 import Staff from "./components/Pages/Staff";
+import Login from "./components/Auth/Login";
+
+const config = {
+  issuer: "https://dev-509456.oktapreview.com/oauth2/default",
+  redirect_uri: window.location.origin + "/implicit/callback",
+  client_id: "0oagjltwv1BekJ1980h7"
+};
+
+const onAuthRequired = ({ history }) => {
+  history.push("/login");
+};
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Route path="/" exact={true} component={Home} />
-            <Route path="/staff" exact={true} component={Staff} />
+        <Security
+          issuer={config.issuer}
+          client_id={config.client_id}
+          redirect_uri={config.redirect_uri}
+          onAuthRequired={onAuthRequired}
+        >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl="https://dev-509456.oktapreview.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </div>
           </div>
-        </div>
+        </Security>
       </Router>
     );
   }
